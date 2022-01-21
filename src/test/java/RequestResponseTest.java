52d2787c53d6e329e1ac1636f36000824a6147d2
tkhuyen <tkhuyen1212@gmail.com>
2022-01-21 23:47:35 +0700

    request-response

--- src/test/java/RequestResponseTest.java
+++ src/test/java/RequestResponseTest.java
@@ -7,20 +7,16 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 
-public class RequestResponseTest extends APITest{
+public class RequestResponseTest extends  APITest{
 
     @Test
-    public void getRequest(){
+    public void getQuestion(){
         String link = "/api/requests";
         WebTarget requestTarget = client.target(BaseURL + link);
-        ArrayList<Object> requests = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get(new GenericType<ArrayList<Object>>() {
-        });
-        Assert.assertTrue(requests.size() > 15);
-
         Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
         int actualResponseCode = response.getStatus();
         int expectedResponseCode = 200;
         Assert.assertEquals(actualResponseCode,expectedResponseCode);
-
     }
 }
+
