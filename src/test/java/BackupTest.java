e9afc88f91a94a7af9fe3775c099bbc72ffe8db2
nguyennhung1529 <nguyenthinhung1529@gmail.com>
2022-01-25 17:01:03 +0700

    update BackupTest

--- src/test/java/BackupTest.java
+++ src/test/java/BackupTest.java
@@ -15,24 +15,27 @@ public class BackupTest extends APITestClass{
         int expectedResponseCode = 200;
         Assert.assertEquals(expectedResponseCode,actualResponseCode);
     }
+
     @Test
     public void restoreBackup(){
         String link = "/api/restore";
-        WebTarget requestTarget = client.target(BaseURL + link).queryParam("filename","abc");
+        WebTarget requestTarget = client.target(BaseURL + link).path("abc");
         Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
         int actualResponseCode = response.getStatus();
         int expectedResponseCode = 200;
         Assert.assertEquals(expectedResponseCode,actualResponseCode);
     }
+
     @Test
-    public void downloadBackup(){
+    public void downBackup(){
         String link = "/api/backup/download";
-        WebTarget requestTarget = client.target(BaseURL + link).queryParam("filename","abc");
+        WebTarget requestTarget = client.target(BaseURL + link).path("abc");
         Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
         int actualResponseCode = response.getStatus();
         int expectedResponseCode = 200;
         Assert.assertEquals(expectedResponseCode,actualResponseCode);
     }
+
     @Test
     public void createBackup(){
         String link = "/api/backup/create";
