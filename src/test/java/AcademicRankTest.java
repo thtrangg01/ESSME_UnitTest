82e6bb0cdddaa2fb055d4ef8e83d97190513f052
nguyennhung1529 <nguyenthinhung1529@gmail.com>
2022-01-25 16:43:15 +0700

    update class AcademicRankTest  Add 3 methods for 3 API GET

--- src/test/java/AcademicRankTest.java
+++ src/test/java/AcademicRankTest.java
@@ -1,2 +1,41 @@
-public class AcademicRankTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class AcademicRankTest extends APITestClass{
+    @Test
+    public void getAcademicRank(){
+        String link = "/api/academic-rank";
+        WebTarget requestTarget = client.target(BaseURL + link)
+                .queryParam("_id", "61965f211440add5d12283fe");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+    @Test
+    public void getAcademicRanks(){
+        String link = "/api/academic-rank";
+        WebTarget requestTarget = client.target(BaseURL + link)
+                .queryParam("page", 1)
+                .queryParam("size", 20)
+                .queryParam("asc", "true");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+    @Test
+    public void searchAcademicRanks(){
+        String link = "/api/academic-rank/search";
+        WebTarget requestTarget = client.target(BaseURL + link)
+                .queryParam("text", "giáo sư");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
 }
