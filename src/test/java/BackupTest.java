5dc0efc9a2b77e717eafde268911f2a60e3781d5
nguyennhung1529 <nguyenthinhung1529@gmail.com>
2022-01-25 16:26:27 +0700

    update content in BackupTest  Add 4 methods

--- src/test/java/BackupTest.java
+++ src/test/java/BackupTest.java
@@ -1,2 +1,45 @@
-public class BackupTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class BackupTest extends APITestClass{
+    @Test
+    public void getBackup(){
+        String link = "/api/backup";
+        WebTarget requestTarget = client.target(BaseURL + link);
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+    @Test
+    public void restoreBackup(){
+        String link = "/api/restore";
+        WebTarget requestTarget = client.target(BaseURL + link).queryParam("filename","abc");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+    @Test
+    public void downloadBackup(){
+        String link = "/api/backup/download";
+        WebTarget requestTarget = client.target(BaseURL + link).queryParam("filename","abc");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+    @Test
+    public void createBackup(){
+        String link = "/api/backup/create";
+        WebTarget requestTarget = client.target(BaseURL + link);
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
 }
