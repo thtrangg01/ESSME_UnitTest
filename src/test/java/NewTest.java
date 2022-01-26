d0420e073062339c6c98a92da3d647e0b672f724
thtrangg <nguyenthutrang01bn@gmail.com>
2022-01-26 23:22:30 +0700

    testUniversities

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -9,10 +9,27 @@ import java.util.ArrayList;
 
 public class NewTest extends APITestClass {
     @Test
-    public void search(){
+    public void getNew(){
+        String link = "/api/news";
+        WebTarget requestTarget = client.target(BaseURL + link)
+                .queryParam("limit","abc");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+        Assert.assertTrue(response.readEntity(String.class).length()>0);
+    }
+
+    @Test
+    public void createNew(){
+
+    }
+
+    @Test
+    public void searchNew(){
         String link = "/api/news/search";
         WebTarget requestTarget = client.target(BaseURL + link);
-        String json = "{\"what\":\"it\"}";
+        String json = "{\"what\":\"thu+nhap\"}";
 //        ArrayList<Object> news = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).post();
 //        Assert.assertTrue(news.size() > 15);
 
@@ -21,4 +38,21 @@ public class NewTest extends APITestClass {
         int expectedResponseCode = 200;
         Assert.assertEquals(actualResponseCode,expectedResponseCode);
     }
+
+    @Test
+    public void deleteNew(){
+
+    }
+
+    @Test
+    public void updateNew(){
+
+    }
+
+    @Test
+    public void getNewById(){
+
+    }
+
+
 }
--- src/test/java/UniversityTest.java
+++ src/test/java/UniversityTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
@@ -24,5 +25,126 @@ public class UniversityTest extends APITestClass{
         Assert.assertEquals(actualResponseCode,expectedResponseCode);
     }
 
+    @Test
+    public void createUniversity(){
+        String link = "/api/universities";
+        String json = "{\n" +
+                "  \"country\": \"Viet Nam\",\n" +
+                "  \"website\": \"https://www.neu.edu.vn/\",\n" +
+                "  \"types\": [\n" +
+                "    \"abc\"\n" +
+                "  ],\n" +
+                "  \"last_updated\": \"abc\",\n" +
+                "  \"ranks\": {\n" +
+                "    \"the\": 1,\n" +
+                "    \"qs\": 0,\n" +
+                "    \"arwu\": 0\n" +
+                "  },\n" +
+                "  \"names\": {\n" +
+                "    \"vi\": \"Dai hoc Kinh te Quoc dan\",\n" +
+                "    \"en\": \"National Economics University\"\n" +
+                "  },\n" +
+                "  \"synonyms\": {\n" +
+                "    \"de\": [\n" +
+                "      {}\n" +
+                "    ],\n" +
+                "    \"vi\": [\n" +
+                "      {}\n" +
+                "    ],\n" +
+                "    \"en\": [\n" +
+                "      \"abc\"\n" +
+                "    ],\n" +
+                "    \"fr\": [\n" +
+                "      {}\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"name\": \"NEU\",\n" +
+                "  \"id\": \"test01\",\n" +
+                "  \"source\": \"https://www.neu.edu.vn/\"\n" +
+                "}";
+        WebTarget requestTarget = client.target(BaseURL + link);
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE)
+                .post(Entity.json(json));
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 401;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
 
+    @Test
+    public void searchUniversity(){
+        String link = "/api/universities/search";
+        WebTarget requestTarget = client.target(BaseURL + link).queryParam("name","Massachusetts");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+
+    }
+
+    @Test
+    public void updateUniversity(){
+        String link = "/api/universities";
+        String json = "{\n" +
+                "  \"country\": \"Viet Nam\",\n" +
+                "  \"website\": \"https://www.neu.edu.vn/\",\n" +
+                "  \"types\": [\n" +
+                "    \"abc\"\n" +
+                "  ],\n" +
+                "  \"last_updated\": \"abc\",\n" +
+                "  \"ranks\": {\n" +
+                "    \"the\": 1,\n" +
+                "    \"qs\": 0,\n" +
+                "    \"arwu\": 0\n" +
+                "  },\n" +
+                "  \"names\": {\n" +
+                "    \"vi\": \"Dai hoc Kinh te Quoc dan\",\n" +
+                "    \"en\": \"National Economics University\"\n" +
+                "  },\n" +
+                "  \"synonyms\": {\n" +
+                "    \"de\": [\n" +
+                "      {}\n" +
+                "    ],\n" +
+                "    \"vi\": [\n" +
+                "      {}\n" +
+                "    ],\n" +
+                "    \"en\": [\n" +
+                "      \"abc\"\n" +
+                "    ],\n" +
+                "    \"fr\": [\n" +
+                "      {}\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  \"name\": \"NEU\",\n" +
+                "  \"id\": \"test01\",\n" +
+                "  \"source\": \"https://www.neu.edu.vn/\"\n" +
+                "}";
+        WebTarget requestTarget = client.target(BaseURL + link).path("61e68a9cde266dfc4b84b012");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE)
+                .put(Entity.json(json));
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 401;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+
+    @Test
+    public void deleteUniversity(){
+        String link = "/api/universities";
+        WebTarget requestTarget = client.target(BaseURL + link).path("61e68a9cde266dfc4b84b00f");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).delete();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 401;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+    }
+
+    @Test
+    public void findUniversityById(){
+        String link = "/api/universities";
+        WebTarget requestTarget = client.target(BaseURL + link)
+                .path("61e68a9cde266dfc4b84b011");
+        Response response = requestTarget.request(MediaType.APPLICATION_JSON_TYPE).get();
+        int actualResponseCode = response.getStatus();
+        int expectedResponseCode = 200;
+        Assert.assertEquals(expectedResponseCode,actualResponseCode);
+        Assert.assertTrue(response.readEntity(String.class).length()>0);
+    }
 }
